944e53ba7da31d55270364d122bc0609d2e5b62e
NeelOommen <neelittyerahoommen@gmail.com>
2023-07-14 13:31:38 +0530

    favicon fix 5

--- influplat/app/layout.js
+++ influplat/app/layout.js
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/public/deco.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Poppins:wght@700&display=swap" rel="stylesheet" />
